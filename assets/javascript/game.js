7036f8cc4ef5360ddfb0c3ecfdf9224bbd157645
Yash <yash592@gmail.com>
2017-11-08 22:07:29 -0800

    works in console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,12 +33,33 @@
   	// console.log(trainTime);
   	// console.log(frequency);
   	// // console.log(trainName);
+     var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+
+     console.log(firstTimeConverted);
+
+     var currentTime = moment('hhmm');
+     console.log(currentTime);
+     var currentTimehhmm = moment(currentTime).format("hh:mm")
+     console.log(currentTimehhmm);
+
+     var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+     console.log(timeDiff);
+     var timeremainder = timeDiff % frequency;
+     console.log(timeremainder);
+
+     var miunutesTillTrain = frequency - timeremainder;
+     console.log(miunutesTillTrain);
+
+     var nextTrain = moment().add(miunutesTillTrain, "minutes");
+     console.log("arrival time: " + moment(nextTrain).format("hh:mm"));
 
   	database.ref().push({
   		trainName: trainName,
   		destination: destination,
   		trainTime: trainTime,
   		frequency: frequency,
+      // nextTrain: nextTrain,
+      // miunutesTillTrain: miunutesTillTrain,
   	});
   	
 
@@ -46,11 +67,30 @@
 
   });
 
+  // var firstTime = "03:30"
+
+ 
+
+  // var now = moment();
+
+  // console.log(now);
+
+  // console.log(moment().format());
+
+//   var time = new Date();
+// console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
   database.ref().on("child_added", function(trainAdded) {
 
   	// Need a way to get the current time
+
+//   var time = new Date();
+// console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
   	// get train times using first train time and frequency
   	// using that calculate the frequency and append all 3 variables below.
+
+
 
      $("#table").append('<tr><td>' + trainName + '</td><td>' + destination + '</td><td>' + frequency + '</td></tr>')
 
